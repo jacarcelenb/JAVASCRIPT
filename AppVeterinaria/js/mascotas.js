3bdc4c692cb909426cee1bbcd2e8f12ca8e94397
jacarcelenb <carcelenjorge17@gmail.com>
2021-09-06 17:36:52 -0500

    Se modifico el archivo mascotas.js

--- AppVeterinaria/js/mascotas.js
+++ AppVeterinaria/js/mascotas.js
@@ -1,6 +1,11 @@
 
 const listaMascotas = document.getElementById("lista-mascotas");
-console.log("Mascotas");
+const nombre = document.getElementById("nombre");
+const propietario = document.getElementById("propietario");
+const tipoanimal = document.getElementById("tipoanimal");
+const form = document.getElementById("form");
+const btnGuardar = document.getElementById('btn-guardar');
+const indiceEditar = document.getElementById('indice');
 
 let mascotas = [
     {
@@ -13,7 +18,7 @@ let mascotas = [
 function listarMascotas() {
     // la funcion map recorre el arreglo y ejecuta el callback
     // funcion join para evitar que los elementos hmtl se junten
-    let htmlMascotas = mascotas.map((mascota , indice) => `
+    let htmlMascotas = mascotas.map((mascota, indice) => `
     <tr>
     <th scope="row">${indice}</th>
     <td>${mascota.tipo}</td>
@@ -21,7 +26,8 @@ function listarMascotas() {
     <td>${mascota.propietario}</td>
     <td>
         <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-            <button type="button" class="btn btn-warning"><i class="fa fa-bars"
+            <button type="button" class="btn btn-warning editar" data-bs-toggle="modal"
+            data-bs-target="#exampleModal"><i class="fa fa-bars"
                     aria-hidden="true"></i></button>
             <button type="button" class="btn btn-danger"><i class="fa fa-window-close-o"
                     aria-hidden="true"></i></button>
@@ -29,6 +35,60 @@ function listarMascotas() {
         </div>
     </td>
 </tr>`).join("");
+    listaMascotas.innerHTML = htmlMascotas;
+    Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index) =>
+        botonEditar.onclick = editar(index))
 }
 
+function enviarDatos(evento) {
+    evento.preventDefault();
+    const datos = {
+        tipo: tipoanimal.value,
+        nombre: nombre.value,
+        propietario: propietario.value
+
+    };
+
+    const accion = btnGuardar.innerHTML;
+
+    switch (accion) {
+        case 'Editar':
+            // editar 
+            mascotas[indiceEditar.value] = datos;
+            break;
+
+        default:
+            // crear
+            mascotas.push(datos);
+            break;
+    }
+
+
+
+    listarMascotas();
+    resetModal();
+
+}
+// patron closure
+function editar(id) {
+    btnGuardar.innerText = 'Editar';
+    $('#exampleModal').modal('toggle')
+    return function handler() {
+        const mascota = mascotas[id];
+        nombre.value = mascota.nombre;
+        propietario.value = mascota.propietario;
+        tipoanimal.value = mascota.tipo;
+        indiceEditar.value = id;
+    }
+}
+
+function resetModal() {
+    nombre.value = "";
+    propietario.value ="";
+    tipoanimal.value ="";
+    indiceEditar.value = "";
+    btnGuardar.innerText = 'Crear';
+}
 listarMascotas();
+form.onsubmit = enviarDatos;
+btnGuardar.onclick = enviarDatos;
