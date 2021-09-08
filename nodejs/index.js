c88a05bd059b406056eebafa117faca36be77b56
jacarcelenb <carcelenjorge17@gmail.com>
2021-09-08 16:37:19 -0500

    Se creo un servidor http en node js

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,2 +1,10 @@
-console.log("hola mundo");
+//console.log("hola mundo");
 // comando node seguido del nombre del archivo.js ejecuta ese archivo en node js
+const http = require('http');
+const server = http.createServer((req ,res)=>{
+    res.end();
+});
+server.on('clientError',(err,socket)=>{
+    socket.end('HTTP/1.1 400 Bad Request \r\n\r\n');
+});
+server.listen(8000);
