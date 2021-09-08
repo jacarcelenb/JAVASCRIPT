9672cba27b9cfd84fba14b5b1e807637a5cbfeff
jacarcelenb <carcelenjorge17@gmail.com>
2021-09-08 16:42:49 -0500

    Se creo un mensaje para mostrar el funcionamiento del servidor

--- nodejs/index.js
+++ nodejs/index.js
@@ -7,4 +7,6 @@ const server = http.createServer((req ,res)=>{
 server.on('clientError',(err,socket)=>{
     socket.end('HTTP/1.1 400 Bad Request \r\n\r\n');
 });
-server.listen(8000);
+server.listen(8000 , () =>{
+    console.log('server listen in http://localhost:8000/');
+});
