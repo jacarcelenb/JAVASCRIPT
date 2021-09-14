abfedb692e941fcd21db9c4d2229d55503d010ba
jacarcelenb <carcelenjorge17@gmail.com>
2021-09-13 21:01:24 -0500

    Solucion de errores en el archivo server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -30,74 +30,72 @@ const server = http.createServer((req, res) => {
   // obtener variables del query url
   const { query = {} } = urlParseada;
   // obtener los headers
-  const { headers } = req;
+  const { headers = {} } = req;
   // obtener payload en el caso de haber uno
   const decoder = new StringDecoder('utf-8');
   let buffer = "";
-  req.on('data', (data) => {
+  req.on("data", (data) => {
     buffer += decoder.write(data);
 
   });
 
-  req.on('end', () => {
+  req.on("end", () => {
     buffer += decoder.end();
 
-
-    if (headers['Content-Type'] == 'application/json') {
-      buffer = JSON.parse(buffer);
+    if (headers["content-type"] === 'application/json') {
+      buffer=JSON.parse(buffer);
     }
+
+    const data = {
+      ruta: rutaLimpia,
+      metodo,
+      query,
+      headers,
+      payload: buffer
+    }
+    console.log({ data })
+
+
+    const enrutador = {
+      ruta: (data, callback) => {
+        callback(200, { mensaje: "esta es /ruta" });
+      },
+      mascotas: {
+        get: (data, callback) => {
+          callback(200, recursos.mascotas);
+        },
+        post: (data, callback) => {
+          recursos.mascotas.push(data.payload);
+          callback(201, data.payload);
+        },
+      },
+      noEncontrado: (data, callback) => {
+        callback(404, { mensaje: "no encontrado" });
+      }
+    }
+
+
+
+    // elegir el manejador de la respuesta
+    let handler;
+    if (data.ruta && enrutador[data.ruta] && enrutador[data.ruta][metodo]) {
+      handler = enrutador[data.ruta][metodo];
+    } else {
+      handler = enrutador.noEncontrado;
+    }
+
+    // ejecutar handler para enviar la respuesta
+    if (typeof handler == 'function') {
+      handler(data, (status = 200, mensaje) => {
+        const respuesta = JSON.stringify(mensaje);
+        res.setHeader('Content-Type', 'application/json')
+        res.writeHead(status)
+        // linea donde realmente se responde a la aplicacion cliente
+        res.end(respuesta)
+      })
+    }
+
   });
-
-  
-
-  const data = {
-    ruta: rutaLimpia,
-    metodo,
-    query,
-    headers,
-    payload: buffer
-  }
-
-  const enrutador = {
-    ruta: (data, callback) => {
-      callback(200, { mensaje: "esta es /ruta" });
-    },
-    mascotas: {
-      get: (data, callback) => {
-        callback(200, recursos.mascotas);
-      },
-      post: (data, callback) => {
-        recursos.mascotas.push(data.payload);
-        callback(201, data.payload);
-      },
-    },
-    noEncontrado: (data, callback) => {
-      callback(404, { mensaje: "no encontrado" });
-    }
-  }
-
-
-
-  // elegir el manejador de la respuesta
-  let handler;
-  if (data.ruta && enrutador[data.ruta] && enrutador[data.ruta][metodo]) {
-    handler = enrutador[data.ruta][metodo];
-  } else {
-    handler = enrutador.noEncontrado;
-  }
-
-  // ejecutar handler para enviar la respuesta
-  if (typeof handler == 'function') {
-    handler(data, (status = 200, mensaje) => {
-      const respuesta = JSON.stringify(mensaje);
-      res.setHeader('Content-Type', 'application/json')
-      res.writeHead(status)
-      // linea donde realmente se responde a la aplicacion cliente
-      res.end(respuesta)
-    })
-  }
-
-console.log(data)
 
 });
 
