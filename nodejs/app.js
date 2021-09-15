12c55ace1c1615559fb1dbf3a1d2dd92286e1c5b
jacarcelenb <carcelenjorge17@gmail.com>
2021-09-15 18:35:35 -0500

    Cambio en el servidor http

--- nodejs/app.js
+++ nodejs/app.js
@@ -100,6 +100,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, () => {
-  console.log('server listen in http://localhost:8500/');
+  console.log('server listen in http://localhost:3500/');
 });
 
