f3879a2e325471682267b3f0dfe4e92725f0e55d
jacarcelenb <carcelenjorge17@gmail.com>
2021-09-15 18:09:35 -0500

    Cambio de puerto

--- nodejs/server.js
+++ nodejs/server.js
@@ -100,6 +100,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, () => {
-  console.log('server listen in http://localhost:8000/');
+  console.log('server listen in http://localhost:8010/');
 });
 
