6c254c9139c846aa580eb541b35b7c232a773878
jacarcelenb <carcelenjorge17@gmail.com>
2021-09-15 18:36:03 -0500

    Cambio en el archivo index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -94,6 +94,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, () => {
-  console.log('server listen in http://localhost:8000/');
+  console.log('server listen in http://localhost:8400/');
 });
 
