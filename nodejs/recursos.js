41c983a447b43390c1ef25fe76c0469f35acb578
jacarcelenb <carcelenjorge17@gmail.com>
2021-09-16 07:41:10 -0500

    Creacion de los recursos para los veterinarios

--- nodejs/recursos.js
+++ nodejs/recursos.js
@@ -5,5 +5,11 @@ module.exports = {
         { tipo: "perro", nombre: "Trusky", propietario: "Camilo" },
         { tipo: "perro", nombre: "Trusky", propietario: "Jorge" }
     ],
+    veterinarios: [
+        {nombre: "Lucia", apellido: "Mina" , documento: "1005400184"},
+        {nombre: "Ana", apellido: "Casa" , documento: "1005400190"},
+        {nombre: "Jose", apellido: "Alvarez" , documento: "1235400164"},
+        {nombre: "James", apellido: "Mena" , documento: "1987400184"},
+    ],
 
 }
