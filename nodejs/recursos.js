d439859b633c847e398bbb68de9ace72d8784575
jacarcelenb <carcelenjorge17@gmail.com>
2021-09-16 06:13:46 -0500

    Creacion del archivo recursos

---
+++ nodejs/recursos.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mascotas: [
+        { tipo: "perro", nombre: "Trusky", propietario: "Camilo" },
+        { tipo: "perro", nombre: "Trusky", propietario: "Camilo" },
+        { tipo: "perro", nombre: "Trusky", propietario: "Camilo" },
+        { tipo: "perro", nombre: "Trusky", propietario: "Jorge" }
+    ],
+
+}
