b89aaa0cccc83b967d5317587272755d07b50032
jacarcelenb <carcelenjorge17@gmail.com>
2021-09-16 07:40:45 -0500

    Creacion de la ruta veterinarios

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -1,11 +1,13 @@
 
 const recursos = require('./recursos');
 const mascotas = require("./rutas/mascotas")
+const veterinarios = require("./rutas/veterinarios")
 module.exports = {
   ruta: (data, callback) => {
     callback(200, { mensaje: "esta es /ruta" });
   },
   mascotas: mascotas(recursos.mascotas),
+  veterinarios: veterinarios(recursos.veterinarios),
   noEncontrado: (data, callback) => {
     callback(404, { mensaje: "no encontrado" });
   }
