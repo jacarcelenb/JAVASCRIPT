01128f2f8c89f153006df37aecb2ab9b2e18e88f
jacarcelenb <carcelenjorge17@gmail.com>
2021-09-16 18:13:59 -0500

    Se añadio la ruta de los dueños de mascotas

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -2,12 +2,14 @@
 const recursos = require('./recursos');
 const mascotas = require("./rutas/mascotas")
 const veterinarios = require("./rutas/veterinarios")
+const propietarios = require("./rutas/propietarios")
 module.exports = {
   ruta: (data, callback) => {
     callback(200, { mensaje: "esta es /ruta" });
   },
   mascotas: mascotas(recursos.mascotas),
   veterinarios: veterinarios(recursos.veterinarios),
+  propietarios: propietarios(recursos.propietarios),
   noEncontrado: (data, callback) => {
     callback(404, { mensaje: "no encontrado" });
   }
