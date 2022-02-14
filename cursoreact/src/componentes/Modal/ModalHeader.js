109c93127ae09a7a8a2fa960e5b5b99533bbaa7b
jacarcelenb <carcelenjorge17@gmail.com>
2022-02-14 17:00:27 -0500

    Creacion de nuevos componentes

---
+++ cursoreact/src/componentes/Modal/ModalHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ModalHeader() {
+    <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Nueva Mascota</h5>
+
+        <button id="btn-closemodal" type="button" className="btn-close" data-bs-dismiss="modal"
+            aria-label="Close"></button>
+    </div>
+}
+
+export default ModalHeader
--- cursoreact/src/componentes/Modal/index.js
+++ cursoreact/src/componentes/Modal/index.js
@@ -1,64 +1,59 @@
 import React from "react";
+import ModalHeader from "./ModalHeader";
 
 
 function Modal() {
- return ( <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-             <div class="modal-content"> 
-               <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Nueva Mascota</h5>
+    return (<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+            <div className="modal-content">
+                <ModalHeader></ModalHeader>
+                <div className="modal-body">
+                    <form id="form">
+                        <div className="col">
+                            <label for="exampleInputEmail1" className="form-label">Nombre</label>
+                            <input type="text" className="form-control" id="nombre" />
 
-                        <button id="btn-closemodal" type="button" class="btn-close" data-bs-dismiss="modal"
-                            aria-label="Close"></button> 
+                        </div>
+
+                        <br />
+                        <div className="col">
+                            <select className="form-control" id="propietario" aria-label="Default select example">
+                                <option>Propietario</option>
+                                <option>Esteban</option>
+                                <option>Gabriela</option>
+                                <option>Pamela</option>
+                                <option>Otto</option>
+                                <option>Jorge</option>
+                                <option>Camilo</option>
+
+                            </select>
+
+                        </div>
+                        <br />
+                        <div className="col">
+                            <select className="form-control" id="tipoanimal" aria-label="Default select example">
+                                <option>Tipo de animal</option>
+                                <option>Perro</option>
+                                <option>Gato</option>
+                                <option>Pajaro</option>
+                                <option>Otro</option>
+                            </select>
+                        </div>
+
+
+                    </form>
+
                 </div>
-                <div class="modal-body">
-                 <form id="form"> 
-                  <input type="hidden" id="indice"/>
-                            <div class="col">
-                                <label for="exampleInputEmail1" class="form-label">Nombre</label>
-                                <input type="text" class="form-control" id="nombre"/>
-
-                            </div>
-
-                             <br />
-                            <div class="col">
-                                <select class="form-control" id="propietario" aria-label="Default select example">
-                                    <option>Propietario</option>
-                                    <option>Esteban</option>
-                                    <option>Gabriela</option>
-                                    <option>Pamela</option>
-                                    <option>Otto</option>
-                                    <option>Jorge</option>
-                                    <option>Camilo</option>
-
-                                </select>
-
-                            </div>
-                            <br />
-                            <div class="col">
-                                <select class="form-control" id="tipoanimal" aria-label="Default select example">
-                                    <option>Tipo de animal</option>
-                                    <option>Perro</option>
-                                    <option>Gato</option>
-                                    <option>Pajaro</option>
-                                    <option>Otro</option>
-                                </select>
-                            </div>
-
-                 
-                 </form>
-                
+                <div className="modal-footer">
+                    <button id="btn-cancelar" type="button" className="btn btn-secondary"
+                        data-bs-dismiss="modal">Cancelar</button>
+                    <button id="btn-guardar" type="button" data-bs-dismiss="modal"
+                        className="btn btn-primary">Guardar</button>
                 </div>
-                <div class="modal-footer">
-                        <button id="btn-cancelar" type="button" class="btn btn-secondary"
-                            data-bs-dismiss="modal">Cancelar</button>
-                        <button id="btn-guardar" type="button" data-bs-dismiss="modal"
-                            class="btn btn-primary">Guardar</button>
-                    </div>
-                
-             </div>
+
             </div>
-        </div>);
+        </div>
+    </div>);
 }
 
 export default Modal;
