ad7733ceb0f834f30c71dc3855bcb4dcef26ed65
jacarcelenb <carcelenjorge17@gmail.com>
2022-02-25 20:44:35 -0500

    Cambios en el index de la modal

--- cursoreact/src/componentes/Modal/index.js
+++ cursoreact/src/componentes/Modal/index.js
@@ -7,52 +7,51 @@ import "./Modal.css"
 function Modal() {
 
     return (
-    <>
-    <div className="modal">
-        <div className="modal-dialog modal-dialog-centered">
-            <div className="modal-content">
-                <ModalHeader></ModalHeader>
-                <div className="modal-body">
-                    <form id="form-row">
-                        <div className="col">
-                            <label for="exampleInputEmail1" className="form-label">Nombre</label>
-                            <input type="text" className="form-control" id="nombre" />
+        <>
+            <div className="modal">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <ModalHeader />
+                        <div className="modal-body">
+                            <form id="form">
+                                <Select />
+                                <div className="form-row">
+                                    <div className="col">
+                                        <input type="text"
+                                            className="form-control"
+                                            id="nombre" />
+
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <select className="form-control" id="propietario" aria-label="Default select example">
+                                        <option>Propietario</option>
+                                        <option>Esteban</option>
+                                        <option>Gabriela</option>
+                                        <option>Pamela</option>
+                                        <option>Otto</option>
+                                        <option>Jorge</option>
+                                        <option>Camilo</option>
+
+                                    </select>
+
+                                </div>
+
+                            </form>
 
                         </div>
-
-                        <br />
-                        <div className="col">
-                            <select className="form-control" id="propietario" aria-label="Default select example">
-                                <option>Propietario</option>
-                                <option>Esteban</option>
-                                <option>Gabriela</option>
-                                <option>Pamela</option>
-                                <option>Otto</option>
-                                <option>Jorge</option>
-                                <option>Camilo</option>
-
-                            </select>
-
+                        <div className="modal-footer">
+                            <button id="btn-cancelar" type="button" className="btn btn-secondary"
+                                data-bs-dismiss="modal">Cancelar</button>
+                            <button id="btn-guardar" type="button" data-bs-dismiss="modal"
+                                className="btn btn-primary">Guardar</button>
                         </div>
-                        <br />
-                        
-                      <Select></Select>
 
-                    </form>
-
+                    </div>
                 </div>
-                <div className="modal-footer">
-                    <button id="btn-cancelar" type="button" className="btn btn-secondary"
-                        data-bs-dismiss="modal">Cancelar</button>
-                    <button id="btn-guardar" type="button" data-bs-dismiss="modal"
-                        className="btn btn-primary">Guardar</button>
-                </div>
-
             </div>
-        </div>
-    </div>
-     <div className="modal-backdrop fade show"></div>
-    </>);
+            <div className="modal-backdrop fade show"></div>
+        </>);
 }
 
 export default Modal;
