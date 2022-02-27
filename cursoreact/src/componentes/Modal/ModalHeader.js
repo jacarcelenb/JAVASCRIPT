2c6f2b05c9c0a191a65639f59babdf84502c09de
jacarcelenb <carcelenjorge17@gmail.com>
2022-02-26 20:07:52 -0500

    Cambios en el header de la modal

--- cursoreact/src/componentes/Modal/ModalHeader.js
+++ cursoreact/src/componentes/Modal/ModalHeader.js
@@ -6,9 +6,7 @@ return(
     <div className="modal-header">
     <h5 className="modal-title" id="exampleModalLabel">Nueva Mascota</h5>
 
-    <button id="btn-closemodal" type="button" className="close" data-bs-dismiss="modal"
-        aria-label="Close"
-        onClick={cambiarModal}></button>
+    <button id="btn-closemodal" type="button" className="close"onClick={cambiarModal}>X</button>
 </div>
 )
 }
